272ada337d03021bf9a2ecb4676036ee8e1eb446
oriya <k-oriya@cec-ltd.co.jp>
2018-03-19 13:01:12 +0900

    update revert process

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -35,9 +35,16 @@
 (setq completion-ignore-case t)
 (setq eww-search-prefix "https://www.google.co.jp/search?q=")
 
-(defun revert-buffer-no-confirm ()
-    "Revert buffer without confirmation."
-    (interactive) (revert-buffer t t))
+(defun revert-buffer-no-confirm (&optional force-reverting)
+  "Interactive call to revert-buffer. Ignoring the auto-save
+ file and not requesting for confirmation. When the current buffer
+ is modified, the command refuses to revert it, unless you specify
+ the optional argument: force-reverting to true."
+  (interactive "P")
+  ;;(message "force-reverting value is %s" force-reverting)
+  (if (or force-reverting (not (buffer-modified-p)))
+      (revert-buffer :ignore-auto :noconfirm)
+    (error "The buffer has been modified")))
 
 (global-set-key (kbd "<f5>") 'revert-buffer-no-confirm)
 
