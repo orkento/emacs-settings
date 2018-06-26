c043d5fa59470be88686f7c4e3c21b097c9e5a57
orkento <k-oriya@mksc.jp>
2018-06-26 18:04:43 +0900

    remove company-semantic

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -45,11 +45,12 @@
 (setq custom-file (locate-user-emacs-file "custom.el"))
 
 (require 'company)
-(add-hook 'after-init-hook 'global-company-mode)
+(global-company-mode)
 (setq company-idle-delay 0)
 (setq company-minimum-prefix-length 2)
 (setq company-selection-wrap-around t)
 (setq company-dabbrev-downcase nil)
+(delete 'company-semantic company-backends)
 (define-key company-active-map (kbd "C-n") 'company-select-next)
 (define-key company-active-map (kbd "C-p") 'company-select-previous)
 
